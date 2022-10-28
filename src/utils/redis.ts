655b35c1e0eaa3de4b41ab9ccc24e7d652d390f3
jordan claypole <jphoneacc98@gmail.com>
2022-10-28 15:28:58 +0100

    redis configured

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -3,12 +3,22 @@ import type { RedisClientType } from "redis";
 let redisClient: RedisClientType | null = null;
 redisClient =
   redisClient ||
-  createClient({
-    socket: {
-      host: "127.0.0.1",
-      port: 6379,
-    },
-  });
+  createClient(
+    process.env.NODE_ENV === "development"
+      ? {
+          socket: {
+            host: "127.0.0.1",
+            port: 6379,
+          },
+        }
+      : {
+          url: process.env.REDIS_URL,
+          socket: {
+            tls: true,
+            rejectUnauthorized: false,
+          },
+        }
+  );
 /*redisClient.on("error", (err) => {
 });*/
 export default redisClient;
